e936845ed77fd436b0c28ca58071ee9db680b637
Manuel Peuster <manuel@peuster.de>
2025-11-17 21:40:30 +0100

    some cleanup and additions

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-ln -f ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -f ~/dotfiles/vimrc ~/.vimrc
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -f tmux.conf ~/.tmux.conf
+
+# vim
+ln -f vimrc ~/.vimrc
+
+# k9s
+# theme pack
+OUT="${XDG_CONFIG_HOME:-$HOME/Library/Application Support}/k9s/skins"
+mkdir -p "$OUT"
+curl -L https://github.com/catppuccin/k9s/archive/main.tar.gz | tar xz -C "$OUT" --strip-components=2 k9s-main/dist
+# config
+ln -f k9s-config.yaml "${XDG_CONFIG_HOME:-$HOME/Library/Application Support}/k9s/config.yaml"
