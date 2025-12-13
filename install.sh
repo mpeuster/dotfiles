fc355ce8637b36c8bc2251527042143bd57188ac
Manuel Peuster <manuel@peuster.de>
2025-12-13 18:14:12 +0100

    Update

--- install.sh
+++ install.sh
@@ -14,3 +14,11 @@ mkdir -p "$OUT"
 curl -L https://github.com/catppuccin/k9s/archive/main.tar.gz | tar xz -C "$OUT" --strip-components=2 k9s-main/dist
 # config
 ln -f k9s-config.yaml "${XDG_CONFIG_HOME:-$HOME/Library/Application Support}/k9s/config.yaml"
+
+# supporting tools for NVIM setup
+npm install tree-sitter-cli
+brew install fzf
+brew install ripgrep
+brew install fd
+
+
